4a12641379059b71929a6bf9a2707658a8a1af18
ImCreator2 <ravi4uever07@gmail.com>
2019-01-03 21:37:36 +0530

    #3 Commit

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -81,12 +81,12 @@ public final class R {
         public static final int packed = 0x7f070057;
         public static final int parent = 0x7f070058;
         public static final int percent = 0x7f07005a;
-        public static final int right = 0x7f07005e;
-        public static final int spread = 0x7f070076;
-        public static final int spread_inside = 0x7f070077;
-        public static final int start = 0x7f07007c;
-        public static final int top = 0x7f07008b;
-        public static final int wrap = 0x7f070091;
+        public static final int right = 0x7f070060;
+        public static final int spread = 0x7f070079;
+        public static final int spread_inside = 0x7f07007a;
+        public static final int start = 0x7f07007f;
+        public static final int top = 0x7f07008e;
+        public static final int wrap = 0x7f070095;
     }
     public static final class styleable {
         private styleable() {}
